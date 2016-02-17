71a06ed2547288f13a2934711bace945c606f448
Marcos <velozmarkdrea@gmail.com>
2016-02-17 17:40:46 +0100

    Making Boostrap able to execute a controller given a url param

--- src/Coolframework/Component/Bootstrap/Bootstrap.php
+++ src/Coolframework/Component/Bootstrap/Bootstrap.php
@@ -3,18 +3,26 @@
 namespace Coolframework\Component\Bootstrap;
 
 use Coolframework\Component\Routing\Routing;
-use Symfony\Component\Yaml\Parser;
 
 class Bootstrap
 {
+	private $routing;
+
 	public function __construct()
 	{
-		echo "Hello, I'm the Coolframework";
 	}
 
 	public function configRouting($routing_to_config_file)
 	{
-		$routing = new Routing();
-		$routing->setRoutingDirectory($routing_to_config_file);
+		$this->routing = new Routing();
+		$this->routing->setRoutingDirectory($routing_to_config_file);
+	}
+
+	public function execute($actual_url)
+	{
+		$controller_to_instantiate = $this->routing->getController($actual_url);
+		$action_to_execute = $this->routing->getAction($actual_url);
+		$current_controller = new $controller_to_instantiate();
+		$current_controller->$action_to_execute();
 	}
 }
