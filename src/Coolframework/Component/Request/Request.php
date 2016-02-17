2673cf84a893cac3ae94743def96c67a042fa9cb
Marcos <velozmarkdrea@gmail.com>
2016-02-17 20:29:35 +0100

    Setting my new Request Component

---
+++ src/Coolframework/Component/Request/Request.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Coolframework\Component\Request;
+
+class Request
+{
+	private $full_raw_url;
+
+	public function __construct($a_full_raw_url)
+	{
+		$this->full_raw_url = $a_full_raw_url;
+	}
+
+	public function fullRawUrl()
+	{
+		return $this->full_raw_url;
+	}
+
+	public function params($index_from_array)
+	{
+		$url_in_array = explode("/", parse_url($this->full_raw_url)['path']);
+		if (array_key_exists($index_from_array, $url_in_array))
+		{
+			return $url_in_array[ $index_from_array ];
+		}
+
+		return 'index';
+	}
+}
