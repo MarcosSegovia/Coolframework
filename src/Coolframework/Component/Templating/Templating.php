74a476fc09d833ff5262ef316a6169df362a4e93
Marcos Segovia <velozmarkdrea@gmail.com>
2016-02-21 18:04:30 +0100

    Example of Smarty initialization without injection

--- src/Coolframework/Component/Templating/Templating.php
+++ src/Coolframework/Component/Templating/Templating.php
@@ -4,16 +4,33 @@ namespace Coolframework\Component\Templating;
 
 class Templating
 {
-	public function __construct($name_of_template_engine )
-	{
+	private $template_engine;
 
+	public function __construct($name_of_template_engine)
+	{
+		if ('smarty' === $name_of_template_engine)
+		{
+			$this->template_engine = $this->configSmartyTemplateEngine();
+		}
+		else
+		{
+			$this->template_engine = $this->configTwigTemplateEngine();
+		}
 	}
 
 	private function configSmartyTemplateEngine()
 	{
+		$smarty = new Smarty();
+
+		$smarty->setTemplateDir('/web/www.example.com/smarty/templates');
+		$smarty->setCompileDir('/web/www.example.com/smarty/templates_c');
+		$smarty->setCacheDir('/web/www.example.com/smarty/cache');
+
+		return $smarty;
 	}
 
 	private function configTwigTemplateEngine()
 	{
+
 	}
 }
