7af6a05a7425ae482cb9338cb5b5fe321a0b8d5b
Marcos Segovia <velozmarkdrea@gmail.com>
2016-02-21 17:51:58 +0100

    Setting Templating in the framework

---
+++ src/Coolframework/Component/Templating/Templating.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Coolframework\Component\Templating;
+
+class Templating
+{
+	public function __construct($name_of_template_engine )
+	{
+
+	}
+
+	private function configSmartyTemplateEngine()
+	{
+	}
+
+	private function configTwigTemplateEngine()
+	{
+	}
+}
