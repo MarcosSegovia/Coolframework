e04279b967cdcbf6fbc456e684b4796dc1ce6719
Marcos <velozmarkdrea@gmail.com>
2016-02-22 20:27:49 +0100

    Redefining Interface for Templating

--- src/Coolframework/Component/Templating/Templating.php
+++ src/Coolframework/Component/Templating/Templating.php
@@ -2,45 +2,7 @@
 
 namespace Coolframework\Component\Templating;
 
-use Smarty;
-use Twig_Environment;
-use Twig_Loader_Filesystem;
-
-class Templating
+interface Templating
 {
-	private $template_engine;
-
-	public function __construct($name_of_template_engine)
-	{
-		if ('smarty' === $name_of_template_engine)
-		{
-			$this->template_engine = $this->configSmartyTemplateEngine();
-		}
-		else
-		{
-			$this->template_engine = $this->configTwigTemplateEngine();
-		}
-	}
-
-	private function configSmartyTemplateEngine()
-	{
-		$smarty = new Smarty();
-
-		$smarty->setTemplateDir('/web/www.example.com/smarty/templates');
-		$smarty->setCompileDir('/web/www.example.com/smarty/templates_c');
-		$smarty->setCacheDir('/web/www.example.com/smarty/cache');
-
-		return $smarty;
-	}
-
-	private function configTwigTemplateEngine()
-	{
-		$loader = new Twig_Loader_Filesystem('/path/to/templates');
-		$twig = new Twig_Environment($loader, array(
-			'cache' => '/path/to/compilation_cache',
-		));
-
-		return $twig;
-	}
-
+	public function render($template_to_render, $array_with_key_value);
 }
