0046ea68bed7134220af7218ea8044e47467f464
Marcos <velozmarkdrea@gmail.com>
2016-02-28 14:10:12 +0100

    Defining Interface for All Persistence Systems

---
+++ src/Coolframework/Component/Persistence/PersistenceClient.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Coolframework\Component\Persistence;
+
+interface PersistenceClient
+{
+	public function prepare($statement_to_prepare);
+
+	public function bindValue($parameter, $value);
+
+	public function execute();
+
+	public function query(
+		$query_to_execute,
+		$array_with_values_to_bind = null
+	);
+
+	public function select(
+		$table,
+		$array_of_condition_value = null
+	);
+
+	public function insert(
+		$table,
+		$array_of_column_value
+	);
+
+	public function update(
+		$table,
+		$array_of_column_value
+	);
+
+	public function delete(
+		$table,
+		$array_of_condition_value = null
+	);
+}
